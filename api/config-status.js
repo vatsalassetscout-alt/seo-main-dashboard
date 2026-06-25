ba7390c0eca1ef9ba9ec4d09cf3e775b09caa544
vatsalassetscout-alt <vatsal.assetscout@gmail.com>
2026-06-25 18:42:26 +0530

    Update config-status.js

--- api/config-status.js
+++ api/config-status.js
@@ -1,4 +1,4 @@
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -8,6 +8,14 @@ module.exports = async (req, res) => {
   const privateKey = process.env.GOOGLE_PRIVATE_KEY;
   const spreadsheetId = process.env.GOOGLE_SHEET_ID;
 
+  const leadsClientEmail = process.env.GOOGLE_LEADS_CLIENT_EMAIL || clientEmail;
+  const leadsPrivateKey = process.env.GOOGLE_LEADS_PRIVATE_KEY || privateKey;
+  const leadsSpreadsheetId = process.env.GOOGLE_LEADS_SHEET_ID;
+
+  const gscClientEmail = process.env.GOOGLE_GSC_CLIENT_EMAIL || clientEmail;
+  const gscPrivateKey = process.env.GOOGLE_GSC_PRIVATE_KEY || privateKey;
+  const gscSpreadsheetId = process.env.GOOGLE_GSC_SHEET_ID || spreadsheetId;
+
   return res.status(200).json({
     configured: !!(clientEmail && privateKey && spreadsheetId),
     hasEmail: !!clientEmail,
@@ -15,5 +23,17 @@ module.exports = async (req, res) => {
     hasSheetId: !!spreadsheetId,
     sheetId: spreadsheetId || null,
     clientEmail: clientEmail || null,
+
+    leadsConfigured: !!(leadsClientEmail && leadsPrivateKey && leadsSpreadsheetId),
+    hasLeadsSpecificEmail: !!process.env.GOOGLE_LEADS_CLIENT_EMAIL,
+    hasLeadsSpecificKey: !!process.env.GOOGLE_LEADS_PRIVATE_KEY,
+    leadsSheetId: leadsSpreadsheetId || null,
+    leadsClientEmail: leadsClientEmail || null,
+
+    gscConfigured: !!(gscClientEmail && gscPrivateKey && gscSpreadsheetId),
+    hasGscSpecificEmail: !!process.env.GOOGLE_GSC_CLIENT_EMAIL,
+    hasGscSpecificKey: !!process.env.GOOGLE_GSC_PRIVATE_KEY,
+    gscSheetId: gscSpreadsheetId || null,
+    gscClientEmail: gscClientEmail || null,
   });
-};
+}
